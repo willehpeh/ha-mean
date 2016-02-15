071b33a9e127ef49b640c402c1ffc5cb24c2a73c
Will <will.alexander@me.com>
2016-02-15 18:56:33 +0100

    Created back end authentication and tested

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  secret : 'pRivat3-k3Y-gO3s-h3R3',
+  secret : 'h4_4Rch1t3ctur3_pR1v4t3_k3Y_gO3s_h3R3',
   db_url :  'mongodb://will:will@ds055905.mongolab.com:55905/wills-test-db'
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,123 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var User = mongoose.model("User");
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
 
-/*
+var config = require('../config/config');
 
-TODO : INTEGRATE AUTHENTICATION
+// Allow new users to sign up
+router.route('/signup')
+  .post(function(req, res, next) {
 
-*/
+    // Retrieve username and password from request
+    var username = req.body.username;
+    var password = req.body.password;
+
+    // Check if user already exists
+    User.findOne({ name: username }, function(err, user) {
+      if(err) {
+        return res.status(500).send({success: false, message: "Database error."});
+      }
+      if(user) {
+        return res.status(401).send({success: false, message: "User already exists."});
+      }
+      else {
+        // Create new user
+        var user = new User();
+        user.name = username;
+
+        // Create password hash and store to db
+        var passwordHash = crypto
+          .createHmac('sha256', config.secret)
+          .update(password)
+          .digest('hex');
+        user.password = passwordHash;
+
+        user.save(function(err, user) {
+          if(err) {
+            return res.status(500).send({success: false, message: "Database error."});
+          }
+        });
+
+        // Create and serve token
+        var token = jwt.sign(user, config.secret, {
+          expiresIn: 3600 // expires in 60 minutes
+        });
+        return res.status(200).send({success: true, token: token, message: "Authentication successful."});
+      }
+    });
+  });
+
+router.route('/login')
+  .post(function(req, res, next) {
+
+    // Retrieve username and password from request
+    var username = req.body.username;
+    var password = req.body.password;
+
+    // Create password hash using secret key
+    var passwordHash = crypto
+      .createHmac('sha256', config.secret)
+      .update(password)
+      .digest('hex');
+
+    // Attempt to authenticate user
+    User.findOne({ name: username }, function(err, user) {
+      if(err) {
+        return res.status(500).send({success: false, message: "Database error."});
+      }
+      if(!user) {
+        return res.status(401).send({success: false, message: "Invalid username."});
+      }
+      if(user.password != passwordHash) {
+        return res.status(401).send({success: false, message: "Incorrect password."});
+      }
+
+      // Username and password correct, supply token
+      if(user.password == passwordHash) {
+        var token = jwt.sign(user, config.secret, {
+          expiresIn: 3600 // expires in 60 minutes
+        });
+        return res.status(200).send({success: true, token: token, message: "Authentication successful."});
+      }
+    });
+  });
+
+// ==================================================================================================
+
+//                                ROUTE FOR TEST PURPOSES ONLY
+//                                REMOVE FOR DEPLOYMENT
+
+// ==================================================================================================
+
+router.route('/checktoken')
+  .post(function(req, res, next) {
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    // decode token
+    if (token) {
+
+      // verifies secret and checks exp
+      jwt.verify(token, config.secret, function(err, decoded) {
+        if (err) {
+          return res.status(403).send({ success: false, message: 'Failed to authenticate token.' });
+        } else {
+          // if everything is good, save to request for use in other routes
+          return res.status(200).send({ success: true, token: decoded, message: "Token verified."});
+        }
+      });
+
+    } else {
+
+      // if there is no token
+      // return an error
+      return res.status(403).send({
+          success: false,
+          message: 'No token provided.'
+      });
+    }
+  });
 
 module.exports = router;
