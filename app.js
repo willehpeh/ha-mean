143aa4eee33643ad9d753df3da3df0d50cd03867
Will <will.alexander@me.com>
2016-02-15 17:47:00 +0100

    Added crypto and jsonwebtoken

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
 
 var config = require('./config/config');
 
