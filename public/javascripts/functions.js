ecce055823f2a98b0bdbc122bc50db0f5f338250
Will <will.alexander@me.com>
2016-02-15 17:26:42 +0100

    Fixed overlay fading

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -1,8 +1,15 @@
 $('.menu-button').click(function() {
   $('.overlay').fadeIn();
+  $('#menu').children().each(function(i) {
+    var self = $(this);
+    setTimeout(function() {
+      self.fadeIn(1000);
+    }, i*50);
+  });
 });
 $('.close-button').click(function() {
   $('.overlay').fadeOut();
+  $('#menu').children().fadeOut();
 })
 
 $(document).ready(function() {
