dfeed459a5639b5c832628ab703bec84490c213c
Will <will.alexander@me.com>
2016-02-21 17:28:00 +0100

    Added functionality to New Post

--- public/javascripts/controllers/DashboardCtrl.js
+++ public/javascripts/controllers/DashboardCtrl.js
@@ -1,7 +1,9 @@
 angular.module('ha-mean-angular', ['angular-storage']).controller('DashboardCtrl', DashboardCtrl);
 
 function DashboardCtrl($scope, $http, store, $window) {
+
   $scope.title = "Dashboard";
+
   $http.get('/api/projects/').then(
     function(data) {
       $scope.projects = data.data;
@@ -9,15 +11,30 @@ function DashboardCtrl($scope, $http, store, $window) {
       console.log(data);
     }
   );
-  $http.get('/api/news').then(
-    function(data) {
+
+  var getNews = function() {
+    $http.get('/api/news').then(function(data) {
       $scope.posts = data.data;
     }, function(data) {
       console.log(data);
-    }
-  );
+    });
+  }
+  getNews();
+
   $scope.logout = function() {
     store.remove('token');
     $window.location.href = "/auth/login";
+  }
+  $scope.saveNewPost = function() {
+    if($scope.newPost.title && $scope.newPost.text) {
+      $http.post('/api/news', $scope.newPost).then(function() {
+        $scope.newPost = {};
+        $('.dashboard-form').hide();
+        $('.dashboard-overlay').fadeOut();
+        getNews();
+      });
+    } else {
+      console.log("Missing data.");
+    }
   }
 }
