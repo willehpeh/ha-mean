89f3d824e97632fa486eda998a0d424d2e1c21c1
William Alexander <williamalexander@pc19.home>
2016-03-30 23:41:33 +0200

    First prep for deployment

--- config/config.js
+++ config/config.js
@@ -4,6 +4,6 @@ module.exports = {
   secret : 'h4_4Rch1t3ctur3_pR1v4t3_k3Y_gO3s_h3R3',
 
   // MongoDB location
-  db_url :  'mongodb://will:will@ds055905.mongolab.com:55905/wills-test-db'
-  //db_url : 'mongodb://ha-dummy:silverfish93@ec2-50-112-159-133.us-west-2.compute.amazonaws.com:27017/dummyDB'
+  //db_url :  'mongodb://will:will@ds055905.mongolab.com:55905/wills-test-db'
+  db_url : 'mongodb://huysmans:golfss@ec2-54-200-99-243.us-west-2.compute.amazonaws.com:27017/hadb'
 }
