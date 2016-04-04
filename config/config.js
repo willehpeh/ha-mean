29a9de5af2e71d6dfe28c6104a814ce4cec17552
William Alexander <williamalexander@Williams-MacBook-Air.local>
2016-04-04 14:04:27 +0200

    Corrected database

--- config/config.js
+++ config/config.js
@@ -4,6 +4,6 @@ module.exports = {
   secret : 'h4_4Rch1t3ctur3_pR1v4t3_k3Y_gO3s_h3R3',
 
   // MongoDB location
-  db_url :  'mongodb://will:will@ds055905.mongolab.com:55905/wills-test-db'
-  //db_url : 'mongodb://huysmans:golfss@ec2-54-200-99-243.us-west-2.compute.amazonaws.com:27017/'
+  //db_url :  'mongodb://will:will@ds055905.mongolab.com:55905/wills-test-db'
+  db_url : 'mongodb://huysmans:golfss@ec2-54-200-99-243.us-west-2.compute.amazonaws.com:27017/'
 }
